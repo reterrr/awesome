d0b9def535b0f885c98ff7f38dcc7c7e279ee590
veronikavanivska <veronikavanivska2005@gmail.com>
2025-01-16 21:52:21 +0100

    Completed weather service

--- weather/src/main/java/com/example/weather/controllers/WeatherService.java
+++ weather/src/main/java/com/example/weather/controllers/WeatherService.java
@@ -10,14 +10,13 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -29,6 +28,9 @@ public class WeatherService extends WeatherServiceGrpc.WeatherServiceImplBase {
 
     @Value("${HOURLY_URL}")
     private String hourly_base_url;
+
+    @Value("${DAILY_URL}")
+    private String daily_base_url;
 
     @Value("${WEATHER_API}")
     private String api_key;
@@ -108,23 +110,20 @@ public class WeatherService extends WeatherServiceGrpc.WeatherServiceImplBase {
                         .setMain(weatherData.getString("main"));
 
                 JSONObject windData = jsonResponse.getJSONObject("wind");
-                responseBuilder.setWind(Wind.newBuilder()
-                        .setSpeed(windData.getFloat("speed"))
-                        .setDeg(windData.getInt("deg"))
-                        .setGust(windData.optFloat("gust", 0))
-                        .build());
+                responseBuilder.setWind(parseWind(windData));
 
                 responseBuilder.setHumidity(jsonResponse.getJSONObject("main").getInt("humidity"))
                         .setVisibility(jsonResponse.optInt("visibility", 0))
-                        .setSunrise(jsonResponse.getJSONObject("sys").getInt("sunrise"))
-                        .setSunset(jsonResponse.getJSONObject("sys").getInt("sunset"))
-                        .setDt(jsonResponse.getInt("dt"));
+                        .setSunrise(convertUnixToMinutes(jsonResponse.getJSONObject("sys").getInt("sunrise")))
+                        .setSunset(convertUnixToMinutes(jsonResponse.getJSONObject("sys").getInt("sunset")))
+                        .setDt(convertUnixToMinutes(jsonResponse.getInt("dt")));
 
                 return responseBuilder;
             }
         };
 
     }
+
     @Override
     public StreamObserver<HourlyWeatherRequest> getHourlyWeather(StreamObserver<HourlyWeatherResponse> responseObserver) {
         return new StreamObserver<HourlyWeatherRequest>() {
@@ -144,13 +143,14 @@ public class WeatherService extends WeatherServiceGrpc.WeatherServiceImplBase {
 
             @Override
             public void onError(Throwable throwable) {
-
+                responseObserver.onError(throwable);
             }
 
             @Override
             public void onCompleted() {
-
+                responseObserver.onCompleted();
             }
+
             private void fetchHourlyFromAPI(String city, StreamObserver<HourlyWeatherResponse> responseObserver, String cacheKey) {
                 String url = String.format(hourly_base_url, city, api_key);
                 Request request = new Request.Builder().url(url).build();
@@ -175,36 +175,55 @@ public class WeatherService extends WeatherServiceGrpc.WeatherServiceImplBase {
 
             private HourlyWeatherResponse parseHourlyResponse(JSONObject jsonResponse) {
                 HourlyWeatherResponse.Builder responseBuilder = HourlyWeatherResponse.newBuilder();
-                responseBuilder.setCityName(jsonResponse.getJSONObject("city").getString("name"));
-                responseBuilder.setCountry(jsonResponse.getJSONObject("city").getString("country"));
 
+                String cityName = jsonResponse.getJSONObject("city").getString("name");
+                String countryName = jsonResponse.getJSONObject("city").getString("country");
+                responseBuilder.setCityName(cityName);
+                responseBuilder.setCountry(countryName);
 
                 JSONArray hourlyWeatherArray = jsonResponse.getJSONArray("list");
+
+                Map<String, DailyWeather.Builder> dailyWeatherMap = new HashMap<>();
 
                 for (int i = 0; i < hourlyWeatherArray.length(); i++) {
                     JSONObject hourlyWeatherData = hourlyWeatherArray.getJSONObject(i);
 
+                    String date = convertUnixToDate(hourlyWeatherData.getLong("dt"));
+                    String formattedDate = date.substring(0, 10);
+
+                    DailyWeather.Builder dailyWeatherBuilder = dailyWeatherMap.get(formattedDate);
+                    if (dailyWeatherBuilder == null) {
+                        dailyWeatherBuilder = DailyWeather.newBuilder()
+                                .setDate(formattedDate);
+                        dailyWeatherMap.put(formattedDate, dailyWeatherBuilder);
+                    }
+
                     HourlyWeather.Builder hourlyWeatherBuilder = HourlyWeather.newBuilder()
-                            .setDt(hourlyWeatherData.getInt("dt"))
+                            .setDt(convertUnixToMinutes(hourlyWeatherData.getLong("dt")))
                             .setMain(parseMain(hourlyWeatherData.getJSONObject("main")))
                             .setWeather(parseWeatherDescription(hourlyWeatherData.getJSONArray("weather").getJSONObject(0)))
                             .setWind(parseWind(hourlyWeatherData.getJSONObject("wind")))
                             .setPop(hourlyWeatherData.optFloat("pop", 0.0f))
                             .setClouds(parseClouds(hourlyWeatherData.getJSONObject("clouds")))
-                            .setDtTxt(hourlyWeatherData.getString("dt_txt"))
                             .setSys(parseSys(hourlyWeatherData.getJSONObject("sys")));
 
-                    if(hourlyWeatherData.has("rain")){
+                    if (hourlyWeatherData.has("rain")) {
                         hourlyWeatherBuilder.setRain(parseRain(hourlyWeatherData.getJSONObject("rain")));
                     }
                     if (hourlyWeatherData.has("snow")) {
                         hourlyWeatherBuilder.setSnow(parseSnow(hourlyWeatherData.getJSONObject("snow")));
                     }
 
-                    responseBuilder.addDailyWeather(DailyWeather.newBuilder()
-                            .setDate(convertUnixToDate(hourlyWeatherData.getLong("dt")))
-                            .addHourlyWeather(hourlyWeatherBuilder)
-                            .build());
+                    dailyWeatherBuilder.addHourlyWeather(hourlyWeatherBuilder);
+
+                }
+
+                List<String> sortedDates = new ArrayList<>(dailyWeatherMap.keySet());
+                Collections.sort(sortedDates);
+
+                for (String date : sortedDates) {
+                    DailyWeather.Builder dailyWeatherBuilder = dailyWeatherMap.get(date);
+                    responseBuilder.addDailyWeather(dailyWeatherBuilder.build());
                 }
 
                 return responseBuilder.build();
@@ -221,23 +240,6 @@ public class WeatherService extends WeatherServiceGrpc.WeatherServiceImplBase {
                         .setGrndLevel(mainData.getInt("grnd_level"))
                         .setHumidity(mainData.getInt("humidity"))
                         .setTempKf(mainData.getFloat("temp_kf"))
-                        .build();
-            }
-
-            private WeatherDescription parseWeatherDescription(JSONObject weatherData) {
-                return WeatherDescription.newBuilder()
-                        .setId(weatherData.getInt("id"))
-                        .setMain(weatherData.getString("main"))
-                        .setDescription(weatherData.getString("description"))
-                        .setIcon(weatherData.getString("icon"))
-                        .build();
-            }
-
-            private Wind parseWind(JSONObject windData) {
-                return Wind.newBuilder()
-                        .setSpeed(windData.getFloat("speed"))
-                        .setDeg(windData.getInt("deg"))
-                        .setGust(windData.getFloat("gust"))
                         .build();
             }
 
@@ -262,13 +264,173 @@ public class WeatherService extends WeatherServiceGrpc.WeatherServiceImplBase {
                 return Sys.newBuilder().setPod(sysData.getString("pod")).build();
             }
 
-            private String convertUnixToDate(long timestamp) {
-                java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd");
-                java.util.Date date = new java.util.Date(timestamp * 1000);
-                return sdf.format(date);
+        };
+
+    }
+
+    public StreamObserver<ForecastRequest> getForecastWeather(StreamObserver<ForecastResponse> responseObserver) {
+        return new StreamObserver<ForecastRequest>() {
+            @Override
+            public void onNext(ForecastRequest forecastRequest) {
+                String city = forecastRequest.getCity();
+                String cacheKey = WeatherKeys.DAILY.getLabel() + city.toLowerCase();
+
+                String cachedWeatherData = redisTemplate.opsForValue().get(cacheKey);
+
+                if (StringUtils.hasText(cachedWeatherData)) {
+                    responseObserver.onNext(parseForecastResponse(new JSONObject(cachedWeatherData)));
+
+                } else {
+                    fetchDailyFromAPI(city, responseObserver, cacheKey);
+                }
             }
 
+            @Override
+            public void onError(Throwable throwable) {
+                responseObserver.onError(throwable);
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+
+            private void fetchDailyFromAPI(String city, StreamObserver<ForecastResponse> responseObserver, String cacheKey) {
+                String url = String.format(daily_base_url, city, api_key);
+                Request request = new Request.Builder().url(url).build();
+
+                try (Response response = httpClient.newCall(request).execute()) {
+                    if (!response.isSuccessful()) {
+                        responseObserver.onError(new StatusRuntimeException(
+                                Status.INTERNAL.withDescription("Failed to fetch hourly weather data: " + response.message())));
+                        return;
+                    }
+
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                   ForecastResponse forecastResponse = parseForecastResponse(jsonObject);
+
+                    redisTemplate.opsForValue().set(cacheKey, jsonObject.toString(), 5, TimeUnit.MINUTES);
+
+                    responseObserver.onNext(forecastResponse);
+                } catch (IOException e) {
+                    responseObserver.onError(e);
+                }
+            }
+
+            private ForecastResponse parseForecastResponse(JSONObject jsonResponse) {
+                ForecastResponse.Builder forecastResponseBuilder = ForecastResponse.newBuilder();
+
+                String cityName = jsonResponse.getJSONObject("city").getString("name");
+                String countryName = jsonResponse.getJSONObject("city").getString("country");
+                forecastResponseBuilder.setCity(cityName);
+                forecastResponseBuilder.setCountry(countryName);
+
+                JSONArray forecastDayArray = jsonResponse.optJSONArray("list");
+                if (forecastDayArray == null) {
+                    return forecastResponseBuilder.build();
+                }
+
+                Map<String, ForecastDay.Builder> forecastDayMap = new HashMap<>();
+
+                for (int i = 0; i < forecastDayArray.length(); i++) {
+                    JSONObject forecastDayData = forecastDayArray.optJSONObject(i);
+
+                    long dt = forecastDayData.optLong("dt", 0);
+                    String date = convertUnixToDate(dt);
+                    String formattedDate = date.substring(0, 10);
+
+                    ForecastDay.Builder forecastDayBuilder = forecastDayMap.get(formattedDate);
+                    if (forecastDayBuilder == null) {
+                        forecastDayBuilder = ForecastDay.newBuilder()
+                                .setDt(formattedDate);
+                        forecastDayMap.put(formattedDate, forecastDayBuilder);
+                    }
+
+                    forecastDayBuilder.setSunrise(convertUnixToMinutes(forecastDayData.optLong("sunrise", 0)));
+                    forecastDayBuilder.setSunset(convertUnixToMinutes(forecastDayData.optLong("sunset", 0)));
+                    forecastDayBuilder.setTemp(parseTemp(forecastDayData.optJSONObject("temp")));
+                    forecastDayBuilder.setFeelsLike(parseFeelsLike(forecastDayData.optJSONObject("feels_like")));
+                    forecastDayBuilder.setPressure(forecastDayData.optInt("pressure", 0));
+                    forecastDayBuilder.setHumidity(forecastDayData.optInt("humidity", 0));
+                    forecastDayBuilder.setClouds(forecastDayData.getInt("clouds"));
+                    forecastDayBuilder.setRain(forecastDayData.optFloat("rain", 0));
+                    forecastDayBuilder.setSnow(forecastDayData.optFloat("snow", 0));
+
+
+                    JSONArray weatherArray = forecastDayData.optJSONArray("weather");
+                    if (weatherArray != null) {
+                        for (int j = 0; j < weatherArray.length(); j++) {
+                            JSONObject weatherDescription = weatherArray.optJSONObject(j);
+                            if (weatherDescription != null) {
+                                forecastDayBuilder.addWeather(parseWeatherDescription(weatherDescription));
+                            }
+                        }
+                    }
+
+                }
+
+                List<String> sortedDates = new ArrayList<>(forecastDayMap.keySet());
+                Collections.sort(sortedDates);
+
+                for (String date : sortedDates) {
+                    ForecastDay.Builder forecastDayBuilder = forecastDayMap.get(date);
+                    forecastResponseBuilder.addList(forecastDayBuilder.build());
+                }
+
+                return forecastResponseBuilder.build();
+            }
+
+
+            private Temp parseTemp(JSONObject tempData) {
+                return Temp.newBuilder()
+                        .setDay(tempData.optFloat("day", 0.0f))
+                        .setMin(tempData.optFloat("min", 0.0f))
+                        .setMax(tempData.optFloat("max", 0.0f))
+                        .setNight(tempData.optFloat("night", 0.0f))
+                        .setEve(tempData.optFloat("eve", 0.0f))
+                        .setMorn(tempData.optFloat("morn", 0.0f))
+                        .build();
+            }
+
+            private FeelsLike parseFeelsLike(JSONObject feelsLikeData) {
+                return FeelsLike.newBuilder()
+                        .setDay(feelsLikeData.optFloat("day", 0.0f))
+                        .setNight(feelsLikeData.optFloat("night", 0.0f))
+                        .setEve(feelsLikeData.optFloat("eve", 0.0f))
+                        .setMorn(feelsLikeData.optFloat("morn", 0.0f))
+                        .build();
+            }
         };
+
+    }
+
+    private Wind parseWind(JSONObject windData) {
+        return Wind.newBuilder()
+                .setSpeed(windData.getFloat("speed"))
+                .setDeg(windData.getInt("deg"))
+                .setGust(windData.getFloat("gust"))
+                .build();
+    }
+
+    private WeatherDescription parseWeatherDescription(JSONObject weatherData) {
+        return WeatherDescription.newBuilder()
+                .setId(weatherData.getInt("id"))
+                .setMain(weatherData.getString("main"))
+                .setDescription(weatherData.getString("description"))
+                .setIcon(weatherData.getString("icon"))
+                .build();
+    }
+
+    private String convertUnixToMinutes(long timestamp) {
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("HH:mm:ss");
+        java.util.Date date = new java.util.Date(timestamp * 1000);
+        return sdf.format(date);
+    }
+
+    private String convertUnixToDate(long timestamp) {
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date date = new java.util.Date(timestamp * 1000);
+        return sdf.format(date);
     }
 
 }
