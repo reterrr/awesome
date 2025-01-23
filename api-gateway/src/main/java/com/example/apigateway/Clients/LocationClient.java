a16aecd5c0c955494deafcb47b649854c5386293
reterrr <sharonovnikola1@gmail.com>
2025-01-23 09:55:27 +0100

    api gateway getLocation getLocations

--- api-gateway/src/main/java/com/example/apigateway/Clients/LocationClient.java
+++ api-gateway/src/main/java/com/example/apigateway/Clients/LocationClient.java
@@ -1,14 +1,46 @@
 package com.example.apigateway.Clients;
 
 import com.example.apigateway.Client;
-import com.example.generated.LocationServiceGrpc;
+import com.example.generated.*;
 import io.grpc.Channel;
+import io.grpc.stub.StreamObserver;
+
+import java.util.List;
 
 @Client(host = "${location.server.host}",
         port = "${location.server.port}")
 public class LocationClient {
     private static LocationServiceGrpc.LocationServiceBlockingStub blockingStub;
     private static LocationServiceGrpc.LocationServiceStub stub;
+
+    public static Long getLocation(int id) {
+        var request = GetLocationRequest
+                .newBuilder()
+                .setId(id)
+                .build();
+
+        return blockingStub
+                .get(request)
+                .getLocation()
+                .getId();
+    }
+
+    public static List<String> getLocations(Iterable<Long> ids) {
+        var request = GetLocationsRequest
+                .newBuilder()
+                .addAllIds(ids)
+                .build();
+
+        return blockingStub.getLocations(request)
+                .getLocationsList()
+                .stream()
+                .map(Location::getName)
+                .toList();
+    }
+
+    public static void search() {
+        //
+    }
 
     public static void init(Channel channel) {
         blockingStub = LocationServiceGrpc.newBlockingStub(channel);
--- api-gateway/src/main/java/com/example/apigateway/Controllers/LocationController.java
+++ api-gateway/src/main/java/com/example/apigateway/Controllers/LocationController.java
@@ -1,27 +1,36 @@
 package com.example.apigateway.Controllers;
 
+import com.example.apigateway.Clients.LocationClient;
+import com.example.apigateway.Helpers.SecurityUtil;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/locations")
 public class LocationController {
     @GetMapping("/search")
     public void search() {
-
+        //
     }
 
     @GetMapping("/{id}")
-    public void get() {
+    public long get(@PathVariable int id) {
+        return LocationClient.getLocation(id);
+    }
 
+    @GetMapping("/gets")
+    public List<String> gets(@RequestBody List<Long> ids) {
+        return LocationClient.getLocations(ids);
     }
 
     @PostMapping("/{id}")
     public void pin(@PathVariable String id) {
-
+        //
     }
 
     @DeleteMapping("/{id}")
     public void unpin(@PathVariable String id) {
-
+        //
     }
 }
