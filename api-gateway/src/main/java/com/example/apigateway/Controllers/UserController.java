1a3d388827dd77db990054f28f02ee42e8cba04c
reterrr <sharonovnikola1@gmail.com>
2025-01-28 21:51:56 +0100

    delete user

--- api-gateway/src/main/java/com/example/apigateway/Controllers/UserController.java
+++ api-gateway/src/main/java/com/example/apigateway/Controllers/UserController.java
@@ -1,21 +1,22 @@
 package com.example.apigateway.Controllers;
 
-import com.example.apigateway.Clients.WeatherClient;
+import com.example.apigateway.Clients.UserClient;
 import com.example.apigateway.Helpers.SecurityUtil;
+import com.example.apigateway.Responses.Response;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/users")
-public class UserController
-{
-    @GetMapping("/locations")
-    public void locations() {
-
-    }
-
+public class UserController {
     @DeleteMapping("/")
-    public void deleteUser() {
+    public Response<Void> deleteUser() {
+        var response = UserClient.delete(Integer.parseInt(SecurityUtil.getCurrentUserId()));
 
+        var code = response.getCode();
+        var message = response.getMessage();
+
+        return new Response<>(
+                code, message
+        );
     }
 }
